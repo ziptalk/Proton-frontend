24d866ddf7c2b7ea37f2d98c201dcf5113d52145
sooy <ssy67366736@gmail.com>
2024-09-13 16:40:56 +0900

    fix: remove console

--- src/common/utils/formatPercentValue.ts
+++ src/common/utils/formatPercentValue.ts
@@ -1,6 +1,5 @@
 export const formatPercentValue = (value: number) => {
   if (!value) return '0.00';
-  console.log(value);
   // 숫자로 변환 후 소수점 이하 두 자리로 제한
   value = Math.abs(value);
   return value.toFixed(2);
