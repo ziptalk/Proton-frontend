66f79e7024188f8bf8fd57a163ae6025b255ea4c
sooy <ssy67366736@gmail.com>
2024-10-07 13:09:45 +0900

    feat: deposit constract

--- src/contract/deposit.ts
+++ src/contract/deposit.ts
@@ -2,11 +2,13 @@ import { Coin, OfflineDirectSigner } from '@cosmjs/proto-signing';
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
 import { GasPrice } from '@cosmjs/stargate';
 import { OfflineAminoSigner } from '@keplr-wallet/types';
+import { CONTRANCT_INFO } from '../common/constants/CONTRACTINFO';
 
-const rpcEndpoint = 'https://neutron-rpc.publicnode.com/';
+// 1. 사용자가 amount 입력 후 minting → 유저의 neutron 을 amount 만큼 봇 주소로 transfer
+// 2. 유저의 deposit amount를 컨트랙트에 저장
+// 3. amount * 0.8 만큼의 QVE 토큰을 유저의 지갑 주소로 minting
+
 const address = localStorage.getItem('NEUTRONADDRESS');
-const chain_id = 'neutron-1';
-const decimalPlaces = 6;
 
 const formatAmount = (amount: number, decimalPlaces: number): number => {
   const multiplier = Math.pow(10, decimalPlaces);
@@ -16,22 +18,26 @@ const formatAmount = (amount: number, decimalPlaces: number): number => {
 
 export const depositTransfer = async (value: number) => {
   if (!address) return;
-  const formattedAmount = formatAmount(value, decimalPlaces);
+  const formattedAmount = formatAmount(value, CONTRANCT_INFO.decimalPlaces);
   const get_wallet_for_chain = async (): Promise<
     OfflineAminoSigner | OfflineDirectSigner
   > => {
-    const signer = window?.keplr?.getOfflineSigner(chain_id);
+    const signer = window?.keplr?.getOfflineSigner(CONTRANCT_INFO.chain_id);
     if (signer === undefined) {
       throw new Error('Keplr not found');
     }
     return signer;
   };
   // 3. Define the execute message
-  const executeMsg = { Transfer: { amount: formattedAmount } };
+  const msg = {
+    Deposit: {
+      amount: formattedAmount.toString(),
+    },
+  };
   const wallet = await get_wallet_for_chain();
 
   const client = await SigningCosmWasmClient.connectWithSigner(
-    rpcEndpoint,
+    CONTRANCT_INFO.rpcEndpoint,
     wallet,
     {
       gasPrice: GasPrice.fromString('0.025untrn'), // Set appropriate gas price
@@ -46,8 +52,8 @@ export const depositTransfer = async (value: number) => {
   try {
     const result = await client.execute(
       address,
-      'neutron19z78v2lzp5ryyl4t3h67vquhtj7eg6uuk4zv2mh8rhyk02gkm4yq0h2su2',
-      executeMsg,
+      'neutron1v9xhzye2m8vgqaqjnxc8u4a0sn6m7h4vjsj0ux7zrrgndzytp9ns8uj8q2',
+      msg,
       'auto',
       'memo',
       [send_amount]
