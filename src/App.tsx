cbcab53ed29094612d07679a0dfc79ba77dcc3a9
sooy <ssy67366736@gmail.com>
2024-10-07 15:00:54 +0900

    feat: refac tanstack onboarding

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { theme } from './common/styles/theme';
 import { RouterProvider } from 'react-router-dom';
 import router from './common/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyles} />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Global styles={globalStyles} />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
